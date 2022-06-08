47618b8e50f7f12ed9ea554fff69ba8f4d5b6965
KiyoshiAndres <89398401+KiyoshiAndres@users.noreply.github.com>
2022-06-08 23:01:21 +0900

    this file lets functions behave like elements of a pre sheaf

---
+++ SheavesOfFunctions.py
@@ -0,0 +1,35 @@
+import sympy as sym
+
+def SumOfFunctions(function_1, function_2):
+    def sum(a):
+        return function_1(a) + function_2(a)
+    return sum
+
+def SubOfFunctions(function_1, function_2):
+    def sub(a):
+        return function_1(a) - function_2(a)
+    return sub
+
+def ProductOfFunctions(function_1, function_2):
+    def prod(a):
+        return function_1(a) * function_2(a)
+    return prod
+
+def ScalarProduct(scalar, function_2):
+    def prod(a):
+        return scalar * function_2(a)
+    return prod
+
+def basic_function(int):
+    "Basic building block"
+    def function(a):
+        return sym.Matrix([point[int - 1] for point in a])
+    return function
+
+def constant_function(value):
+    def function(a):
+        output = []
+        for point in a:
+            output.append(value)
+        return sym.Matrix(output)
+    return function
